5cfaf7618b1c5ee22a3c4a2067f3a6104299d3ac
Samticode <samti8686@gmail.com>
2024-02-09 09:26:31 +0100

    f

--- python/bubbleSort/bubbleSort.py
+++ python/bubbleSort/bubbleSort.py
@@ -1,12 +1,12 @@
 import random
 
-def bubblesort(elements):
+def bubble_sort(array):
+    array_length = len(array)
     
-    for i in range(len(elements)):
-        for j in range(0, len(elements)-i-1):
-            if elements[j] > elements[j+1]:
-                # Swap if the element found is greater than the next element
-                elements[j], elements[j+1] = elements[j+1], elements[j]
+    for pass_number in range(array_length):
+        for index in range(0, array_length - pass_number - 1):
+            if array[index] > array[index + 1]:
+                array[index], array[index + 1] = array[index + 1], array[index]
 
 
 elements = [random.randint(1, 50) for _ in range(50)]
